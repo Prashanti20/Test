5b849f0b526632c3a56f2b9f0cbaa2216dde6a0d
user <user@prashanti20>
2024-05-18 10:57:17 +0530

    Add a BeforeSuite Add a BeforeTest Add a BeforeClass Add a BeforeMethod Add a AfterMethod

---
+++ Amazon_Functional_Automation/src/test/java/tests/Base.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+	
+	public static WebDriver openChromeBrowser()  {
+	
+		WebDriver driver = new ChromeDriver();
+		return driver ;
+			
+	}
+	
+	public static WebDriver openFirefoxBrowser()  {
+		
+		WebDriver driver = new ChromeDriver();
+		return driver ;
+		
+		
+	}
+	
+	
+public static WebDriver openIEBrowser()  {
+		
+		WebDriver driver = new ChromeDriver();
+		return driver ;
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- Amazon_Functional_Automation/src/test/java/tests/TestClass.java
+++ Amazon_Functional_Automation/src/test/java/tests/TestClass.java
@@ -1,14 +1,37 @@
 package tests;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
 
 public class TestClass {
 
-	HashMap<Integer,String> map = new HashMap<Integer,String>();
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before Suite-TestClass_1");
+	}
 	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test-TestClass_1");
+	}
 	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class-TestClass_1");
+	}
 	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method-TestClass_2");
+	}
+	
+	@AfterMethod
+	public void AfterMethod() {
+		System.out.println("After Method-TestClass_2");
+	}
 	
 	
 	
